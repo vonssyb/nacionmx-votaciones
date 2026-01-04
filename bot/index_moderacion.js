ff6cd22336a75acdc068dd9c2894918f9c076b64
Gonzalez <gonzalez@MacBook-Air-de-Gonzalez.local>
2026-01-04 17:38:45 -0600

    chore: Bump version to v2.3 to force Render deploy

--- bot/index_moderacion.js
+++ bot/index_moderacion.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 // 1. Unbuffered Logger
 const log = (msg) => process.stderr.write(`🟢 [MOD-BOT] ${msg}\n`);
 
-log('Starting Nacion MX MODERATION BOT... (v2.2 - Message Fix)');
+log('Starting Nacion MX MODERATION BOT... (v2.3 - Rol Fix)');
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
