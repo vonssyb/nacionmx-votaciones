a2f7b781b0ae8d02f0263cf9fb819a783ad75aac
Gonzalez <gonzalez@MacBook-Air-de-Gonzalez.local>
2026-01-04 21:00:38 -0600

    fix: DNI public + Discord avatar, revert SA appeal button v4.6

--- bot/index_moderacion.js
+++ bot/index_moderacion.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 // 1. Unbuffered Logger
 const log = (msg) => process.stderr.write(`🟢 [MOD-BOT] ${msg}\n`);
 
-log('Starting Nacion MX MODERATION BOT... (v4.5 - SA Appeals System)');
+log('Starting Nacion MX MODERATION BOT... (v4.6 - DNI Public + Avatar)');
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
