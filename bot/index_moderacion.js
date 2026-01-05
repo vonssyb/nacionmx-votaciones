f10634911a405395bd953967de17c3feba0ebe24
Gonzalez <gonzalez@MacBook-Air-de-Gonzalez.local>
2026-01-04 20:38:49 -0600

    fix: Perfil UnbelievaBoat integration final v4.2

--- bot/index_moderacion.js
+++ bot/index_moderacion.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 // 1. Unbuffered Logger
 const log = (msg) => process.stderr.write(`🟢 [MOD-BOT] ${msg}\n`);
 
-log('Starting Nacion MX MODERATION BOT... (v4.1 - CacheService Hotfix)');
+log('Starting Nacion MX MODERATION BOT... (v4.2 - Perfil UB Fix Final)');
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
