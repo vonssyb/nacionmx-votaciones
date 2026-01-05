b3489c5864f2d69eb141bfb292c239db558309ff
Gonzalez <gonzalez@MacBook-Air-de-Gonzalez.local>
2026-01-04 22:04:01 -0600

    fix: Remove invalid cleanup call crashing bot v5.8

--- bot/index_moderacion.js
+++ bot/index_moderacion.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 // 1. Unbuffered Logger
 const log = (msg) => process.stderr.write(`🟢 [MOD-BOT] ${msg}\n`);
 
-log('Starting Nacion MX MODERATION BOT... (v5.7 - Staff Salary Added)');
+log('Starting Nacion MX MODERATION BOT... (v5.8 - CacheService Fix)');
 const fs = require('fs');
 const path = require('path');
 const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
