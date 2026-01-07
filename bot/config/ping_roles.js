cd00ad948c534c731e8c21561dc4d998904635b0
Gonzalez <gonzalez@MacBook-Air-de-Gonzalez.local>
2026-01-07 11:40:18 -0600

    feat: add Ping Contenido role

--- bot/config/ping_roles.js
+++ bot/config/ping_roles.js
@@ -7,6 +7,7 @@ module.exports = {
         '1458507296958316751': { label: 'Actualizaciones', emoji: '🆕', style: 'Danger' },
         '1458507711938564399': { label: 'Casino', emoji: '🎰', style: 'Secondary' },
         '1458513516913758208': { label: 'Facciones', emoji: '⚔️', style: 'Danger' },
+        '1458515486722625648': { label: 'Contenido', emoji: '🎬', style: 'Secondary' },
         '1458507744725176501': { label: 'Economía', emoji: '💰', style: 'Success' }
     }
 };
